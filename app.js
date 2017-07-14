4104a38a5a7b8d7523c536779d1407876dbfa6de
Thomas Jones <thomasrjones211@gmail.com>
2017-07-14 15:49:02 -0600

    heroku deploy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
+const port = process.env.PORT || 3000
 
 if (process.env.NODE_ENV !== 'test') {
   const logger = require('morgan')
--- seeds/00_clear.js
+++ seeds/00_clear.js
@@ -1,7 +1,8 @@
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('comments').del()
     .then(function() {
       return knex('posts').del();
+    })
   });
-};
